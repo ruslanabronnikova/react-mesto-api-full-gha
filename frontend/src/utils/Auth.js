330987b197bf741eab24e0d6eb9543f3d9de4b83
Ruslana <bronnikovars@yandex.ru>
2023-08-03 15:14:48 +0300

    Правка адреса

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'https://api.bronnokovarsmesto.nomoreparties.co';
+export const BASE_URL = 'https://api.bronnokovarsmesto.nomoreparties.co';
 
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3000';
 export const handleResponse = (res) => {
   if (res.ok) {
     return res.json();
